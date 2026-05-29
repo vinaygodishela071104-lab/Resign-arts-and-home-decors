62610201aed5504838607b146f62b324cac1f232
Eshwar04 <114127387+Eshwar04@users.noreply.github.com>
2026-05-29 09:55:46 +0530

    first commit

--- index.js
+++ index.js
@@ -19,3 +19,52 @@ homeMenu.onclick = (e) => {
     dropdown.classList.toggle("show-dropdown");
   }
 };
+
+const darkBtn = document.querySelector(".dark-mode-btn");
+
+darkBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  // Save mode
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Load saved mode
+window.addEventListener("load", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+});
+
+/* =========================
+   RTL MODE TOGGLE
+========================= */
+
+const rtlBtn = document.querySelector(".rtl-btn");
+
+rtlBtn.addEventListener("click", () => {
+  if (document.documentElement.getAttribute("dir") === "rtl") {
+    document.documentElement.setAttribute("dir", "ltr");
+    localStorage.setItem("direction", "ltr");
+  } else {
+    document.documentElement.setAttribute("dir", "rtl");
+    localStorage.setItem("direction", "rtl");
+  }
+});
+
+// Load saved direction
+window.addEventListener("load", () => {
+  const savedDirection = localStorage.getItem("direction");
+
+  if (savedDirection === "rtl") {
+    document.documentElement.setAttribute("dir", "rtl");
+  } else {
+    document.documentElement.setAttribute("dir", "ltr");
+  }
+});
